7073814c569b705171760499e45f06a20c2ffd67
Николай Тельпуховский <nikolka233@mail.ru>
2026-08-08 12:33:34 +0300

    chore: removed maplibre and deck gl

--- frontend/src/lib/maplibre.ts
+++
@@ -1,4 +0,0 @@
-import { setWorkerUrl } from "maplibre-gl";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
-
-setWorkerUrl(workerUrl);
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./lib/maplibre";
 import "./index.css";
 
 import App from "./App";
