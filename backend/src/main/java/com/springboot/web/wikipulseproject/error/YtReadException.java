9954adea056379b9be8e05f6825c158ef9143879
Михаил Кужанов <Gogol09876@mail.ru>
2026-08-15 18:08:07 +0300

    backend-2: add YtReadException

---
+++ backend/src/main/java/com/springboot/web/wikipulseproject/error/YtReadException.java
@@ -0,0 +1,7 @@
+package com.springboot.web.wikipulseproject.error;
+
+public class YtReadException extends RuntimeException {
+    public YtReadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
