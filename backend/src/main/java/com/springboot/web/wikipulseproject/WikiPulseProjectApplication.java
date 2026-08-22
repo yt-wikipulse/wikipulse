2472ed31db92163942c1c393b3b4aba94f1e3464
Владимир Корякин <vladimirkoryakin9459@gmail.com>
2026-08-22 18:16:14 +0300

    add new scheduler

--- backend/src/main/java/com/springboot/web/wikipulseproject/WikiPulseProjectApplication.java
+++ backend/src/main/java/com/springboot/web/wikipulseproject/WikiPulseProjectApplication.java
@@ -1,15 +1,8 @@
 package com.springboot.web.wikipulseproject;
 
-import com.springboot.web.wikipulseproject.model.TopArticle;
-import com.springboot.web.wikipulseproject.model.TopGeoPlace;
-import com.springboot.web.wikipulseproject.model.TrendPoint;
-import com.springboot.web.wikipulseproject.yt_repo.YtAggregatesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
